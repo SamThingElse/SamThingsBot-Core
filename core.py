59390423dd833c1294bdaa4234cbc6b3968f3a0c
Samuel Sachs <samuel@sachs.me.uk>
2020-04-15 13:54:12 +0200

    Add discord.ext commands - thx to @Bmbus

--- core.py
+++ core.py
@@ -1,36 +1,69 @@
 import discord
+from discord.ext import commands
+import os
+import re
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        self.apikey = ''
-        print('Logged in as') # Just for Debug
-        print(self.user.name) # Just for Debug
-        print(self.user.id) # Just for Debug
-        print('------')
-
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
-
-        if message.content.startswith('!hello'):
-            await message.channel.send('Hello {0.author.mention}'.format(message))
-        if message.content.startswith('!help'): 
-            await message.channel.send('```Hier folgt in Kürze eine (hoffentlich) ausführliche Hilfe-Sektion über alle möglichen Commands...```'.format(message))
-
-    def apitokenreader(self, keyfile):
-        '''
-        Reads an API-Token from any file. Needs a file path as argument.
-
-        Argument: Filepath as String.
-        '''
-
-        with open(keyfile, "r",encoding="UTF-8") as file:
-            self.apikey = file.readline()
-        return self.apikey
-
+bot = commands.Bot(command_prefix='!')
+modulepath = "./modules"
 keyfile = "./settings/dc_api_token.key"
 
-client = MyClient()
-#client.apikeyreader(keyfile)
-client.run(client.apitokenreader(keyfile))
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":wave: :smiley: Hey there!")
+
+@bot.command()
+async def helloworld(ctx):
+    await ctx.send("Hello World!")
+
+@bot.command()
+async def cat(ctx):
+    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title="SamThingsBot", description="The King of the Bots.", color=0x0000FF)
+
+    # give info about you here
+    embed.add_field(name="Author", value="SamThingElse#7553")
+
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+    # give users a link to invite thsi bot to their server
+    embed.add_field(name="Invite", value="[Invite link](https://discordapp.com/api/oauth2/authorize?client_id=522130071801298954&scope=bot)")
+
+    await ctx.send(embed=embed)
+
+bot.remove_command('help')
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="SamThingsBot", description="List of commands are:", color=0x0000FF)
+
+    embed.add_field(name="!greet", value="Gives a nice greet message", inline=False)
+    embed.add_field(name="!helloworld", value='Print "Hello World!"', inline=False)
+    embed.add_field(name="!cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
+    embed.add_field(name="!info", value="Gives a little information about this bot", inline=False)
+    embed.add_field(name="!help", value="Gives this message", inline=False)
+
+    await ctx.send(embed=embed)
+
+def apitokenreader(keyfile):
+    '''
+    Reads an API-Token from any file. Needs a file path as argument.
+
+    Argument: Filepath as String.
+    '''
+
+    with open(keyfile, "r",encoding="UTF-8") as file:
+        apikey = file.readline()
+    return apikey
+
+bot.run(apitokenreader(keyfile))
